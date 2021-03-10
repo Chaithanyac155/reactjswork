4f0a3b966602017220d14510d84c4593adf48064
Chaithanya Kumar <68162279+Chaithanyac155@users.noreply.github.com>
2021-03-10 20:37:54 +0530

    Update FriendsList.js

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ListItems from "./ListItems";
 import Pagination from "react-js-pagination";
 
 class FriendsList extends React.Component {
@@ -11,6 +10,8 @@ class FriendsList extends React.Component {
         text: "",
         flag: false
       },
+      sitem: [],
+      filterflag: false,
       activePage: 1
     };
     this.addItem = this.addItem.bind(this);
@@ -18,7 +19,6 @@ class FriendsList extends React.Component {
     this.handleInput = this.handleInput.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
     this.favouriteItem = this.favouriteItem.bind(this);
-    this.handlePageChange = this.handlePageChange.bind(this);
   }
 
   addItem(e) {
@@ -35,15 +35,20 @@ class FriendsList extends React.Component {
       });
     }
   }
-  searchItem(e) {
-    e.preventDefault();
-    const sitem = this.state.items.filter((val) =>
+
+  searchItem() {
+    const usersPerPage = 4;
+    const lastUser = this.state.activePage * usersPerPage;
+    const firstUser = lastUser - usersPerPage;
+    let arr = this.state.items.slice(firstUser, lastUser);
+    const shitem = arr.filter((val) =>
       val["text"]
         .toLowerCase()
         .includes(this.state.currentItem.text.toLowerCase())
     );
     this.setState({
-      items: sitem
+      filterflag: true,
+      sitem: shitem
     });
   }
 
@@ -52,7 +57,8 @@ class FriendsList extends React.Component {
       currentItem: {
         text: e.target.value,
         flag: false
-      }
+      },
+      filterflag: false
     });
   }
 
@@ -80,11 +86,19 @@ class FriendsList extends React.Component {
     });
   }
 
-  handlePageChange(pageNumber) {
+  handleSetChange(pageNumber) {
     this.setState({ activePage: pageNumber });
   }
 
   render() {
+    const usersPerPage = 4;
+    const lastUser = this.state.activePage * usersPerPage;
+    const firstUser = lastUser - usersPerPage;
+    const displayFriends = this.state.filterflag
+      ? this.state.sitem
+      : this.state.items.slice(firstUser, lastUser);
+    // const displayFriends = this.state.items.slice(firstUser, lastUser);
+
     return (
       <div className="content mx-auto mt-5">
         <h6 className="title">
@@ -98,24 +112,82 @@ class FriendsList extends React.Component {
             value={this.state.currentItem.text}
             onChange={this.handleInput}
           />
-          <span class="material-icons icon" onClick={this.searchItem}>
+          <span className="material-icons icon" onClick={this.searchItem}>
             person_search
           </span>
         </form>
 
-        <ListItems
-          items={this.state.items}
-          deleteItem={this.deleteItem}
-          favouriteItem={this.favouriteItem}
-        />
+        {/* Addfriend ... list......... */}
+
+        <div>
+          {displayFriends.length ? (
+            <>
+              {displayFriends.map((item, index) => (
+                <div>
+                  <hr style={{ margin: 0 }} />
+                  <div className="row">
+                    <div className="col-sm h-25 mt-1 ml-2">
+                      <b>{item.text}</b>
+
+                      <pre>is your friend</pre>
+                    </div>
+
+                    <div className="col-sm icon-style  mt-3">
+                      {item.flag ? (
+                        <span
+                          className="material-icons"
+                          onClick={() => {
+                            this.favouriteItem(index, false);
+                          }}
+                        >
+                          star
+                        </span>
+                      ) : (
+                        <span
+                          className="material-icons"
+                          onClick={() => {
+                            this.favouriteItem(index, true);
+                          }}
+                        >
+                          star_outline
+                        </span>
+                      )}
+                      <span
+                        className="material-icons"
+                        onClick={() => {
+                          this.deleteItem(index);
+                        }}
+                      >
+                        delete_outline
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <>
+              <p className="ml-2 mt-2">Not Found in List</p>
+            </>
+          )}
+        </div>
+
         <hr />
+        {/* pagination......... */}
+
         <div className="pagination-div">
           <Pagination
             activePage={this.state.activePage}
             itemsCountPerPage={4}
             totalItemsCount={this.state.items.length}
-            pageRangeDisplayed={5}
-            onChange={this.handlePageChange}
+            pageRangeDisplayed={7}
+            hideFirstLastPages={"false"}
+            prevPageText={"Prev"}
+            nextPageText={"Next"}
+            activeLinkClass={"paginationActive"}
+            itemClassPrev={"prev-style"}
+            itemClassNext={"next-style"}
+            onChange={(pageNumber) => this.handleSetChange(pageNumber)}
           />
         </div>
       </div>
